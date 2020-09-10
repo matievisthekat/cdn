ffe4a067524836b472774689496f756b1652ac13
MatievisTheKat <matievisthekat@gmail.com>
2020-09-10 12:23:00 +0200

    add sharex support

--- src/index.js
+++ src/index.js
@@ -7,24 +7,19 @@ const { join } = require("path");
 const fileTypes = ["png", "zip", "jpg", "jpeg"];
 
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, join(__dirname, "../public/"));
-	},
-	filename: function (req, file, cb) {
-		const date = new Date();
-		cb(
-			null,
-			`${date.getDay()}-${date.getMonth()}-${date.getFullYear()}-${shortid.generate()}.${file.originalname
-				.split(".")
-				.pop()}`
-		);
-	},
+  destination: function (req, file, cb) {
+    cb(null, join(__dirname, "../public/"));
+  },
+  filename: function (req, file, cb) {
+    const date = new Date();
+    cb(null, `${file.originalname}_${shortid.generate()}.${file.originalname.split(".").pop()}`);
+  },
 });
 const upload = multer({
-	dest: join(__dirname, "../public/"),
-	preservePath: true,
-	fileFilter,
-	storage,
+  dest: join(__dirname, "../public/"),
+  preservePath: true,
+  fileFilter,
+  storage,
 });
 const app = express();
 const port = 3000;
@@ -37,30 +32,29 @@ app.set("views", join(__dirname, "../views"));
 app.set("json spaces", 2);
 
 app.get("/", (req, res) => {
-	const uploaded = req.query.uploaded;
-	if (uploaded) return res.render("uploaded", { uploaded });
-	res.render("index");
+  const uploaded = req.query.uploaded;
+  if (uploaded) return res.render("uploaded", { uploaded });
+  res.render("index");
 });
 
 app.post("/upload", upload.single("upload"), async (req, res) => {
-	const file = req.file;
-	const jsonFile = join(__dirname, "../public/json.json");
-	const content = await fs.readFile(jsonFile, {
-		encoding: "utf-8",
-	});
-	const json = JSON.parse(content);
-	json.data.push(`/${file.filename}`);
-	await fs.writeFile(jsonFile, JSON.stringify(json), { encoding: "utf-8" });
+  const file = req.file;
+  const jsonFile = join(__dirname, "../public/json.json");
+  const content = await fs.readFile(jsonFile, {
+    encoding: "utf-8",
+  });
+  const json = JSON.parse(content);
+  json.data.push(`/${file.filename}`);
+  await fs.writeFile(jsonFile, JSON.stringify(json), { encoding: "utf-8" });
 
-	res.send(`https://cdn.matievisthekat.dev/${file.filename}`);
-	//res.redirect(`/?uploaded=${file.filename}`);
+  if (req.body.sharex) res.send(`https://cdn.matievisthekat.dev/${file.filename}`);
+  else res.redirect(`/?uploaded=${file.filename}`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 function fileFilter(req, file, cb) {
-	if (!fileTypes.includes(file.originalname.split(".")[1]))
-		return cb(null, false);
+  if (!fileTypes.includes(file.originalname.split(".")[1])) return cb(null, false);
 
-	cb(null, true);
+  cb(null, true);
 }
