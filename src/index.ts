53a24836af081b020b84a78d08d547d4e7bef54b
MatievisTheKat <matievisthekat@gmail.com>
2021-03-01 14:18:34 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { nanoid } from "nanoid";
 import { resolve } from "path";
 import { readdir } from "fs-extra";
 import cors from "cors";
+import bodyparser from "body-parser";
 
 const publicDir = resolve("public");
 const app = express();
@@ -12,8 +13,7 @@ const storage = multer.diskStorage({
     cb(null, publicDir);
   },
   filename: function (req, file, cb) {
-    const date = new Date();
-    cb(null, `${nanoid()}_${nanoid()}.${file.originalname.split(".").pop()}`);
+    cb(null, `${nanoid()}.${file.originalname.split(".").pop()}`);
   },
 });
 const upload = multer({
@@ -25,14 +25,22 @@ const upload = multer({
 app.set("json spaces", 2);
 app.use(express.static(resolve("public")));
 app.use(cors());
+app.use(bodyparser.json());
 
 app.get("/", async (req, res) => {
   const files = await readdir(publicDir);
   res.status(200).json({ files: files.filter((f) => !f.startsWith(".")) });
 });
 
-app.post("/upload", upload.any(), (req, res) => {
-  res.status(201).json({ files: req.files });
+app.post("/upload", upload.array("files"), (req, res) => {
+  const files: Express.Multer.File[] = [];
+
+  for (let i = 0; i < req.files.length; i++) {
+    const file = Array.isArray(req.files) ? req.files[i] : req.files["files"][i];
+    files.push(file);
+  }
+
+  res.status(201).json({ files });
 });
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
